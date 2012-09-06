7544791673ab4d673e0d3e6708d3dd670c9af3df
Andy Davies <andy.davies@gmail.com>
2012-09-06 16:14:19 +0100

    removing hash on end of access_token

--- robohydra/plugins/fbproxy/index.js
+++ robohydra/plugins/fbproxy/index.js
@@ -6,34 +6,29 @@ var heads = require('robohydra').heads,
 exports.getBodyParts = function(config) {
   var projectPath = config.rootpath || '.';
 
+  function output(content) {
+      if (content !== undefined) {
+        console.log("-----------------------------");
+        console.log(content);
+        console.log("-----------------------------");
+      }
+  }
+
   return {
     heads: [
-      new RoboHydraHead({
-        name: "filterfbauthtoken",
-        path: '/fb/graph/.*',
-        handler: function (req, res, next) {
-          var auth_token = req.url.match(/auth_token=(.*)$/)[1];
-          req.url = req.url.replace(auth_token, auth_token + '#_=_');
-          
-          next(req, res);
-        }
-      }),
-
       new RoboHydraHeadWatchdog({
           // Note that res.body is always uncompressed, even if
           // the original response was compressed. If you really
           // want the original body, try "res.rawBody".
           watcher: function(req, res) { },
           reporter: function(req, res) {
-              console.log("-----------------------------");
-              console.log("Response for request to " + req.url + " contained: \n\r" + res.body.toString());
-              console.log("-----------------------------");
+              output("Response for request to " + req.url + " contained: \n\r" + res.body.toString());
           }
       }),
 
       new RoboHydraHeadProxy({
           name: 'proxy',
-          mountPath: '/fb/graph',
+          mountPath: '/fb/graph/',
           proxyTo: 'https://graph.facebook.com/',
           setHostHeader: true
         })
